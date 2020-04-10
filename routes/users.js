88197aedc23a7177900cf11f92f52870e21441f6
snehakr <snehakr044@gmail.com>
2020-04-10 11:43:29 +0530

    first commit

---
+++ routes/users.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+
+  db.query('select * from product', function (err, rows, fields) {
+    if (err) throw err
+
+     res.send(rows); 
+  })
+
+});
+
+router.get('/invoiceList', function(req, res, next) {
+
+  db.query('select * from invoice', function (err, rows, fields) {
+    if (err) throw err
+
+     res.send(rows); 
+  })
+
+});
+
+router.get('/invoice/:id_invoice', function(req, res, next) {
+
+  db.query('select * from invoice where id_invoice='+req.params.id_invoice+'', function (err, rows, fields) {
+    if (err) throw err
+
+     res.send(rows); 
+  })
+
+});
+
+
+router.get('/account_head', function(req, res, next) {
+
+  db.query('select * from account_head', function (err, rows, fields) {
+    if (err) throw err
+
+     res.send(rows); 
+  })
+
+});
+
+
+
+module.exports = router;
