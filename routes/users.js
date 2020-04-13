9b2adf14ea24b3289d59e0e206d7ac0945a9fff7
snehakr <snehakr044@gmail.com>
2020-04-13 17:01:03 +0530

    13/4

--- routes/users.js
+++ routes/users.js
@@ -31,17 +31,36 @@ router.get('/invoice/:id_invoice', function(req, res, next) {
 
 });
 
+
 router.post('/invoice', function(req, res, next) {
-  let invoice_no = req.body.invoice_no;
-  let order_no = req.body.order_no;
+  let invoice_no        = req.body.invoice_no;
+  let order_no          = req.body.order_no;
+  let date              = req.body.date;
+  let buyer_date        = req.body.buyer_date;
+  let exporter          = req.body.exporter;
+  let consignee         = req.body.consignee;
+  let other             = req.body.other;
+  let buyer             = req.body.buyer;
+  let country_origin    = req.body.country_origin;
+  let country_final     = req.body.country_final;
+  let pre_carriage      = req.body.pre_carriage;
+  let receipt_place     = req.body.receipt_place;
+  let vessel_no         = req.body.vessel_no;
+  let port_load         = req.body.port_load;
+  let port_discharge    = req.body.port_discharge;
+  let final_destination = req.body.final_destination;
+  let marks             = req.body.marks;
+  let container_no      = req.body.container_no;
+  let awb_no            = req.body.awb_no;
+  let terms             = req.body.terms;
   
-  db.query(`insert into invoice (invoice_no, order_no) values (${invoice_no},${order_no})`, 
-  function (err, result) {
+  db.query(`insert into invoice (invoice_no, order_no, date, buyer_date, exporter, consignee, other,buyer,country_origin, country_final, pre_carriage, receipt_place, vessel_no,port_load,port_discharge, final_destination, marks, container_no, awb_no, terms) values (${invoice_no},${order_no}, ${date}, ${buyer_date}, ${exporter}, ${consignee}, ${other}, ${buyer}, ${country_origin}, ${country_final}, ${pre_carriage}, ${receipt_place}, ${vessel_no}, ${port_load},${port_discharge}, ${final_destination}, ${marks}, ${container_no}, ${awb_no}, ${terms})`,function (err, result) {
     if (err) throw err;
     
     res.send(result);
   })
 });
+
 
 router.get('/account_head', function(req, res, next) {
 
